45397b7260a0032c114c17cc34103ee7a6b99976
MarcoBorgess <marco.borges@branef.com.br>
2022-08-22 19:20:25 -0300

    forum1

--- linguagem-de-programacao/forum-1.py
+++ linguagem-de-programacao/forum-1.py
@@ -1,12 +1,24 @@
+import random
 
-def getTabuada10(n):
-    print('Tabuada do {}:'.format(n))
-    for i in range(1, 11):
-        r = n*i
-        print(f'{n} x {i} = {r}')
-
-valor = input("Digite um número inteiro: ")
-if type(valor) != int or valor < 0:
-    print('Valor inválido!')
-else:
-    getTabuada10(valor)
+def letra_a():
+    def get_tabuada_10(n):
+        print('Tabuada do {}:'.format(n))
+        for i in range(1, 11):
+            r = n*i
+            print(f'{n} x {i} = {r}')
+        
+    valor = input("Digite um número inteiro: ")
+    if type(valor) != int or valor < 0:
+        print('Valor inválido!')
+    else:
+        get_tabuada_10(valor)
+        
+def letra_b():
+    def embaralhar(str):
+        return ''.join(random.sample(str, len(str))).lower()
+    
+    str = input("Digite uma string: ")
+    str_embaralhada = embaralhar(str)
+    print(str_embaralhada)
+    
+letra_b()
